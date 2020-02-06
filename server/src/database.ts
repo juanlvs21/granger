98fac82a3fce8dc34b853d0cc10cc1fa73e7aafc
Juan Villarroel <juanlvs97@gmail.com>
2020-02-06 14:10:20 -0400

    docker-compose.yml

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
+const URL = process.env.URL_DB || "mongodb://localhost:27017/granger";
+
 mongoose.Promise = global.Promise;
 mongoose
-  .connect("mongodb://localhost/granger", {
+  .connect(URL, {
     useMongoClient: true
   })
   .then(db => console.log("Database is connected"))
